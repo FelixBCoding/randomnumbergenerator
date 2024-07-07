9aa9f78c82911589c10f5af765c62703b04f7b5f
Felixbarrios <Felixbcoding@gmail.com>
2024-07-06 20:07:12 -0400

    First commit

---
+++ RandomNumberGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomNumberGenerator
+//
+//  Created by Felix Barrios on 7/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
